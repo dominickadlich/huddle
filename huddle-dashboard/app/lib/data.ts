c6786778202d2238df772547d92613de966cda5e
Dominick Adlich <139933162+d-ratch@users.noreply.github.com>
2025-08-28 22:15:58 -0500

    Seeded extensiion table and began table

--- huddle-dashboard/app/lib/data.ts
+++ huddle-dashboard/app/lib/data.ts
@@ -78,3 +78,21 @@ export async function fetchLatestOpportunities() {
 }
 
 
+const ITEMS_PER_PAGE = 6;
+export async function fetchFilteredExtensions(
+    query: string,
+    currentPage: number,
+) {
+    const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+
+    try {
+        const { data, error } = await supabase
+        .from('central_directory')
+        .select('*')
+        .order('name')
+        .limit(ITEMS_PER_PAGE)
+    } catch (error) {
+        console.error('Extension database error:', error);
+        throw new Error('Failed to fetch extension data')
+    }
+}
--- huddle-dashboard/app/ui/directory/table.tsx
+++ huddle-dashboard/app/ui/directory/table.tsx
@@ -1,3 +1,37 @@
 import Image from "next/image";
 import { UpdateExtension, DeleteExtension } from "./buttons";
 import { formatDateToLocal } from "@/app/lib/utils";
+import { fetchFilteredExtensions } from "@/app/lib/data";
+
+export default async function ExtensionsTable({
+    query,
+    currentPage,
+}: {
+    query: string;
+    currentPage: number;
+}) {
+    const extensions = await fetchFilteredExtensions(query, currentPage);
+
+    return (
+        <div className="mt-6 flow-root">
+            <div className="inline-block min-w-full align-middle">
+                <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
+                    <div className="md:hidden">
+                        {extensions?.map((extension) => {
+                            <div
+                                key={extension.id}
+                                className="mb-2 w-full rounded-md bg-white p-4"
+                            >
+                                <div className="flex items-center justify-between border-b pb-4">
+                                    <div>
+                                        <p>{extension.name}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
