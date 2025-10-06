4be10f4529bf2609094714625f8f85bdbe8eeb6f
Dominick Adlich <139933162+dominickadlich@users.noreply.github.com>
2025-10-05 21:54:05 -0500

    Began 'shift work data' with form-helpers

--- huddle-dashboard/app/lib/form-helpers.tsx
+++ huddle-dashboard/app/lib/form-helpers.tsx
@@ -14,6 +14,7 @@ export function parseHuddleFormData(formData: FormData) {
     missed_dispense_prep: Number(formData.get("missed_dispense_prep")),
     missed_dispense_check: Number(formData.get("missed_dispense_check")),
     safety: formData.get("safety") || "",
+    
     inventory: formData.get("inventory") || "",
     go_lives: formData.get("go_lives") || "",
     barriers: formData.get("barriers") || "",
@@ -21,5 +22,32 @@ export function parseHuddleFormData(formData: FormData) {
     unresolved_issues: formData.get("unresolved_issues") || "",
     opportunities: formData.get("opportunities") || "",
     shout_outs: formData.get("shout_outs") || "",
+
+    // Morning
+    // inventory_morning: formData.get("inventory") || "",
+    // go_lives_morning: formData.get("go_lives") || "",
+    // barriers_morning: formData.get("barriers") || "",
+    // pass_off_morning: formData.get("pass_off") || "",
+    // unresolved_issues_morning: formData.get("unresolved_issues") || "",
+    // opportunities_morning: formData.get("opportunities") || "",
+    // shout_outs_morning: formData.get("shout_outs") || "",
+
+    // Noon
+    // inventory_noon: formData.get("inventory") || "",
+    // go_lives_noon: formData.get("go_lives") || "",
+    // barriers_noon: formData.get("barriers") || "",
+    // pass_off_noon: formData.get("pass_off") || "",
+    // unresolved_issues_noon: formData.get("unresolved_issues") || "",
+    // opportunities_noon: formData.get("opportunities") || "",
+    // shout_outs_noon: formData.get("shout_outs") || "",
+
+    // Night
+    // inventory_night: formData.get("inventory") || "",
+    // go_lives_night: formData.get("go_lives") || "",
+    // barriers_night: formData.get("barriers") || "",
+    // pass_off_night: formData.get("pass_off") || "",
+    // unresolved_issues_night: formData.get("unresolved_issues") || "",
+    // opportunities_night: formData.get("opportunities") || "",
+    // shout_outs_night: formData.get("shout_outs") || "",
   };
 }
