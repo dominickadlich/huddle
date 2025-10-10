fad71ee5e002dd33d352f532dc3570ea8b2b683d
Dominick Adlich <139933162+dominickadlich@users.noreply.github.com>
2025-10-09 23:51:03 -0500

    Updated all text-box values to shift based safety_night, etc.

--- huddle-dashboard/app/lib/actions.ts
+++ huddle-dashboard/app/lib/actions.ts
@@ -40,14 +40,30 @@ const HuddleFormSchema = z.object({
   missed_dispense_check: z
     .number()
     .min(0, "Please enter missed dispense checks."),
-  safety: z.string(),
-  inventory: z.string(),
-  go_lives: z.string(),
-  barriers: z.string(),
-  pass_off: z.string(),
-  unresolved_issues: z.string(),
-  opportunities: z.string(),
-  shout_outs: z.string(),
+  safety_morning: z.string(),
+  inventory_morning: z.string(),
+  go_lives_morning: z.string(),
+  barriers_morning: z.string(),
+  pass_off_morning: z.string(),
+  unresolved_issues_morning: z.string(),
+  opportunities_morning: z.string(),
+  shout_outs_morning: z.string(),
+  safety_noon: z.string(),
+  inventory_noon: z.string(),
+  go_lives_noon: z.string(),
+  barriers_noon: z.string(),
+  pass_off_noon: z.string(),
+  unresolved_issues_noon: z.string(),
+  opportunities_noon: z.string(),
+  shout_outs_noon: z.string(),
+  safety_night: z.string(),
+  inventory_night: z.string(),
+  go_lives_night: z.string(),
+  barriers_night: z.string(),
+  pass_off_night: z.string(),
+  unresolved_issues_night: z.string(),
+  opportunities_night: z.string(),
+  shout_outs_night: z.string(),
 });
 
 const CreateHuddleReportSchema = HuddleFormSchema.omit({
@@ -80,14 +96,30 @@ export type HuddleState = {
     complex_preps_count?: string[];
     missed_dispense_prep?: string[];
     missed_dispense_check?: string[];
-    safety?: string[];
-    inventory?: string[];
-    go_lives?: string[];
-    barriers?: string[];
-    pass_off?: string[];
-    unresolved_issues?: string[];
-    opportunities?: string[];
-    shout_outs?: string[];
+    safety_morning?: string[];
+    inventory_morning?: string[];
+    go_lives_morning?: string[];
+    barriers_morning?: string[];
+    pass_off_morning?: string[];
+    unresolved_issues_morning?: string[];
+    opportunities_morning?: string[];
+    shout_outs_morning?: string[];
+    safety_noon?: string[];
+    inventory_noon?: string[];
+    go_lives_noon?: string[];
+    barriers_noon?: string[];
+    pass_off_noon?: string[];
+    unresolved_issues_noon?: string[];
+    opportunities_noon?: string[];
+    shout_outs_noon?: string[];
+    safety_night?: string[];
+    inventory_night?: string[];
+    go_lives_night?: string[];
+    barriers_night?: string[];
+    pass_off_night?: string[];
+    unresolved_issues_night?: string[];
+    opportunities_night?: string[];
+    shout_outs_night?: string[];
   };
   message?: string | null;
 };
@@ -118,14 +150,30 @@ export async function createHuddleReport(
     complex_preps_count,
     missed_dispense_check,
     missed_dispense_prep,
-    safety,
-    inventory,
-    go_lives,
-    barriers,
-    pass_off,
-    unresolved_issues,
-    opportunities,
-    shout_outs,
+    safety_morning,
+    inventory_morning,
+    go_lives_morning,
+    barriers_morning,
+    pass_off_morning,
+    unresolved_issues_morning,
+    opportunities_morning,
+    shout_outs_morning,
+    safety_noon,
+    inventory_noon,
+    go_lives_noon,
+    barriers_noon,
+    pass_off_noon,
+    unresolved_issues_noon,
+    opportunities_noon,
+    shout_outs_noon,
+    safety_night,
+    inventory_night,
+    go_lives_night,
+    barriers_night,
+    pass_off_night,
+    unresolved_issues_night,
+    opportunities_night,
+    shout_outs_night,
   } = validatedFields.data;
 
   const currentDate = new Date().toISOString();
@@ -148,14 +196,30 @@ export async function createHuddleReport(
         missed_dispense_prep,
         staffing,
         complex_preps_count,
-        safety,
-        inventory,
-        go_lives,
-        barriers,
-        pass_off,
-        unresolved_issues,
-        opportunities,
-        shout_outs,
+        safety_morning,
+    inventory_morning,
+    go_lives_morning,
+    barriers_morning,
+    pass_off_morning,
+    unresolved_issues_morning,
+    opportunities_morning,
+    shout_outs_morning,
+    safety_noon,
+    inventory_noon,
+    go_lives_noon,
+    barriers_noon,
+    pass_off_noon,
+    unresolved_issues_noon,
+    opportunities_noon,
+    shout_outs_noon,
+    safety_night,
+    inventory_night,
+    go_lives_night,
+    barriers_night,
+    pass_off_night,
+    unresolved_issues_night,
+    opportunities_night,
+    shout_outs_night,
       },
     ]);
 
