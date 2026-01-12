497659f80f9ceb2b5a7ec8f45bc122a376fed439
Dominick Adlich <139933162+dominickadlich@users.noreply.github.com>
2026-01-11 23:16:10 -0600

    Added search bar and table to WDIP page and adjusted page design

--- huddle-dashboard/app/WDIP/page.tsx
+++ huddle-dashboard/app/WDIP/page.tsx
@@ -1,4 +1,5 @@
 import Header from "../ui/header";
+import Search from "../ui/search";
 
 function getCurrentShift(): 'weekday-day' | 'weekday-evening' | 'weekend' {
     const now = new Date();
@@ -40,12 +41,33 @@ export default function Page() {
 
     return (
         <>
-            <Header title="WDIP"/>
-            <div className="flex justify-center text-3xl">
-                Date: {now}
-            </div>
-            <div className="flex justify-center text-3xl mt-10">
-                Viewing Shift: {shift.charAt(0).toUpperCase() + shift.slice(1)}
+            {/* <Header title="WDIP"/> */}
+            <div className="mt-20">
+                <div className="flex justify-center mt-10">
+                    <Search placeholder={"Enter a service"} />
+                </div>
+                {/* <div className="flex justify-center text-3xl">
+                    Date: {now}
+                </div> */}
+                <div className="flex justify-center text-3xl mt-20">
+                    Current Staffing Schedule: {shift.charAt(0).toUpperCase() + shift.slice(1)}
+                </div>
+                <table className="flex justify-center mt-20">
+                    <tbody className="rounded-2xl overflow-hidden border border-gray-400/50">
+                        <tr>
+                        <td className="border-r border-b border-gray-400/50 bg-gray-800/30 py-4 pl-16 pr-16 text-white">Indiana</td>
+                        <td className="border-b border-gray-400/50 bg-gray-800/30 py-4 pl-16 pr-16 text-white">Indianapolis</td>
+                        </tr>
+                        <tr>
+                        <td className="border-r border-b border-gray-400/50 bg-gray-800/30 py-4 pl-16 pr-16 text-white">Ohio</td>
+                        <td className="border-b border-gray-400/50 bg-gray-800/30 py-4 pl-16 pr-16 text-white">Columbus</td>
+                        </tr>
+                        <tr>
+                        <td className="border-r border-gray-400/50 bg-gray-800/30 py-4 pl-16 pr-16 text-white">Michigan</td>
+                        <td className="bg-gray-800/30 py-4 pl-16 pr-16 text-white">Detroit</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </>
     )
