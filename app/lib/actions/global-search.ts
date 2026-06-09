d77a2d9513847e7e27dc0e8d9638cef5653b0d21
Dominick Adlich <139933162+dominickadlich@users.noreply.github.com>
2026-06-08 22:39:50 -0500

    Began global search action in lib/action

---
+++ app/lib/actions/global-search.ts
@@ -0,0 +1,6 @@
+// SELECT 'iv_room' as department, date, summary FROM iv_room WHERE summary ILIKE '%vancomycin%'
+// UNION ALL
+// SELECT 'distribution' as department, date, summary FROM distribution WHERE summary ILIKE '%vancomycin%'
+// UNION ALL
+// SELECT 'command_center' as department, date, summary FROM command_center WHERE summary ILIKE '%vancomycin%'
+// ORDER BY date DESC
