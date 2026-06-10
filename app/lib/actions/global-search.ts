50299f73e6462bb522a537a9d3b377cd84cadd6b
Dominick Adlich <139933162+dominickadlich@users.noreply.github.com>
2026-06-09 22:45:41 -0500

    Created postgresql global_search in supabase and began to flesh out front end plan

--- app/lib/actions/global-search.ts
+++ app/lib/actions/global-search.ts
@@ -1,3 +1,9 @@
+// import { SupabaseAuthClient } from "@supabase/supabase-js/dist/module/lib/SupabaseAuthClient"
+
+// const supabase = SupabaseAuthClient()
+
+// const { data, error } = await supabase.rpc('global_search', { search_term: input });
+
 // SELECT 'iv_room' as department, date, summary FROM iv_room WHERE summary ILIKE '%vancomycin%'
 // UNION ALL
 // SELECT 'distribution' as department, date, summary FROM distribution WHERE summary ILIKE '%vancomycin%'
