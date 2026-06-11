a79892f9c2db4226ad9779c9e94da69a5e513178
Dominick Adlich <139933162+dominickadlich@users.noreply.github.com>
2026-06-10 21:50:04 -0500

    Distribution opportunities generates in summary

--- app/ui/team-huddle/page-client.tsx
+++ app/ui/team-huddle/page-client.tsx
@@ -68,7 +68,8 @@ export default function MiniHuddlePageClient({
             if (fields.barriers) parts.push(`Barriers: ${fields.barriers.toString()}`);
             if (fields.inventory) parts.push(`Inventory: ${fields.inventory.toString()}`)
             if (fields.wins) parts.push(`Wins: ${fields.wins.toString()}`);
-        
+            if (fields.opportunities) parts.push(`Opportunities: ${fields.opportunities.toString()}`);
+
         setEditedSummary(parts.join('\n'));
     }, [fields])
 
