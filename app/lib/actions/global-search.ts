6a2ec668c28374352b1faeb5cdecb25a93e3f4b8
Dominick Adlich <139933162+dominickadlich@users.noreply.github.com>
2026-06-17 22:37:48 -0500

    Enhancing UI of dashboard search modal

--- app/lib/actions/global-search.ts
+++ app/lib/actions/global-search.ts
@@ -1,3 +1,5 @@
+"use server"
+
 import { getAuthenticatedClient } from "../supabase/auth-helpers";
 
 
--- app/ui/global/search-modal.tsx
+++ app/ui/global/search-modal.tsx
@@ -31,26 +31,27 @@ export default function SearchModal({
                 <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                     <DialogPanel
                     transition
-                    className="relative transform overflow-hidden rounded-lg bg-gray-800 px-4 pt-5 pb-4 text-left shadow-xl outline -outline-offset-1 outline-white/10 transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-sm sm:p-6 data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+                    className="relative transform overflow-hidden rounded-lg bg-gray-800 px-4 pt-4 pb-4 text-left shadow-xl outline -outline-offset-1 outline-white/10 transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-4xl sm:p-6 data-closed:sm:translate-y-0 data-closed:sm:scale-95"
                     >
-                        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                            <div className="sm:col-span-4">
-                            <div className="mt-2">
+                        {/* <div className="gap-x-6 gap-y-6"> */}
+                            {/* <div className="sm:col-span-4"> */}
+                            {/* <div className="mt-2"> */}
                                 <div className="flex items-center rounded-md bg-white/5 pl-3 outline-1 -outline-offset-1 outline-white/10 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-500">
-                                <MagnifyingGlassIcon />
+                                <MagnifyingGlassIcon className="h-5 w-5 text-white" />
                                 <input
                                     id="search"
                                     name="search"
                                     type="text"
-                                    placeholder={"Search Huddles"}
+                                    placeholder={"Search huddle summaries"}
                                     onChange={onChange}
                                     value={value}
                                     className="block min-w-0 grow bg-transparent py-1.5 pr-3 pl-1 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-sm/6"
+                                    autoFocus={true}
                                 />
                                 </div>
-                            </div>
-                            </div>
-                        </div>
+                            {/* </div> */}
+                            {/* </div> */}
+                        {/* </div> */}
                     </DialogPanel>
                 </div>
                 </div>
